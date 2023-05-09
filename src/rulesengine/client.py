2f4dc86d69888f4249c8de5a1b58cac017c58e3b
Fabio Sangiovanni <f@sangiovanni.dev>
2023-05-09 11:24:34 +0200

    Add `EngineClient.get_pluggable_props()` method.

--- src/rulesengine/client.py
+++ src/rulesengine/client.py
@@ -9,15 +9,24 @@ class EngineClientError(Exception):
 class EngineClient:
     def __init__(self, base_url, token, timeout=30):
         self._base_url = base_url
-        self._token = token
+        self._headers = {"X-Auth-Token": token}
         self._timeout = timeout
 
     def push_action(self, subject_id: str, action: str, payload: dict) -> None:
         endpoint = f"{self._base_url}/api/v1/subjects/{subject_id}/props/?a={action}"
         data = {"payload": payload}
-        headers = {"X-Auth-Token": self._token}
-        response = requests.post(endpoint, json=data, headers=headers, timeout=self._timeout)
+        response = requests.post(endpoint, json=data, headers=self._headers, timeout=self._timeout)
         try:
             response.raise_for_status()
         except HTTPError as e:
             raise EngineClientError from e
+
+    def get_pluggable_props(self, subject_id: str, props: str, **kwargs):
+        params = {"p": props, **kwargs}
+        endpoint = f"{self._base_url}/api/v1/subjects/{subject_id}/props/"
+        response = requests.get(endpoint, headers=self._headers, params=params, timeout=self._timeout)
+        try:
+            response.raise_for_status()
+        except HTTPError as e:
+            raise EngineClientError from e
+        return response.json()
